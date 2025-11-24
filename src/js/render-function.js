82e9395118e10d40c70183c785a71efc2ee46632
unknown <aquanataliya@gmail.com>
2025-11-25 00:51:24 +0200

    first commit

---
+++ src/js/render-function.js
@@ -0,0 +1,53 @@
+import SimpleLightbox from 'simplelightbox';
+
+export const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+export function createGallery({hits}) {
+    lightbox.refresh();
+    return hits.map(imageTemplate).join('');
+}
+
+function imageTemplate(img){
+    return `<li class="gallery-item">
+                <a class="gallery-link" href="${img.largeImageURL }">
+                    <img
+                        class="gallery-image"
+                        src="${img.webformatURL}"
+                        alt="${img.tags }"
+                    />
+                </a>
+                <ul class="statistic-block">
+                    <li>
+                        <p>Likes</p><span>${img.likes}</span>
+                    </li>
+                    <li>
+                        <p>Views</p><span>${img.views}</span>
+                    </li>
+                    <li>
+                        <p>Comments </p><span>${img.comments}</span>
+                    </li>
+                    <li>
+                        <p>Downloads</p><span>${img.downloads}</span>
+                    </li>
+                </ul>
+            </li>`
+}
+
+function clearGallery() {
+    gallery.innerHTML = '';
+}
+
+export function showLoader() {
+    loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+    loader.classList.add('hidden');
+}
---
+++ src/main.js
@@ -0,0 +1,54 @@
+import iziToast from "izitoast";
+import getImagesByQuery from "./js/pixabay-api";
+import {showLoader, hideLoader, createGallery, gallery } from "./js/render-function";
+
+
+const form = document.querySelector('.form');
+
+const errorText = {
+    user: {
+        title: 'WARNING!',
+        message: 'You didn’t enter the image name!'
+    },
+    api: {
+        title: 'Sorry,',
+        message: 'there are no images matching your search query. Please try again!'}
+    
+}
+
+function validInput({ title, message}) {
+        iziToast.show({
+            title,
+            titleColor: 'red',
+            message,
+            position: 'topCenter',
+            messageColor: 'black',
+            color: 'red',
+    });
+}
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    form.noValidate = true;
+
+    const inputData = new FormData(form);
+    if (inputData.get('search-text').trim() === '') {
+        return validInput(errorText.user);
+    }
+
+    showLoader();
+
+    getImagesByQuery(inputData.get('search-text').trim())
+        .then(({data}) => {
+            if (data.total === 0) return validInput(errorText.api);
+            gallery.innerHTML = createGallery(data);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        })
+        .finally(() =>
+            hideLoader()
+        );
+    
+    ev.target.reset();
+})
